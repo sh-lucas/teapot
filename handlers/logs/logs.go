53229ed65a6c7d955c6f9ee7e70305d6f5f8bf4f
Lucas Schwalm Silva <lucas.schwalm@roxcode.dev>
2026-02-06 12:55:16 -0300

    chore: skip ngrok warning

--- handlers/logs/logs.go
+++ handlers/logs/logs.go
@@ -21,7 +21,8 @@ func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+		// w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type, ngrok-skip-browser-warning")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
